447c6f944b53d7fc3ea853aad7a1aa7dad59cfc6
Daniel Akoto <sult4nbiz@gmail.com>
2022-02-03 21:56:28 -0500

    new update

--- frontend/static/js/views/Dashboard.js
+++ frontend/static/js/views/Dashboard.js
@@ -20,23 +20,6 @@ export default class extends AbstractView {
             <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Magnam recusandae quos repellat deleniti debitis id, earum, dolor corporis sint voluptates nam autem? Blanditiis inventore itaque aspernatur consectetur esse nisi temporibus?</p>
         </div>
         </center>
-        <div class="profile">
-        <center>
-            <div class="whitebox2">
-                <img style="border-radius: 100%; width: 25%; margin-top: 30px;" src="/static/pictures/profile.png" alt="">
-                <h1 style="font-size: 42px;">Example Name</h1>
-                
-                <h3 style=" margin-left: 4%; font-size: 22px; margin-bottom: 5px;"> Example Occupation </h3>
-                <h3 style=" margin-left: 4%; ">Example University </h3>
-                <br>
-                <p>Example bio... Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quas quos quidem in dolore! Perspiciatis, sed repudiandae similique illo doloremque quia.  Lorem ipsum dolor sit amet consectetur adipisicing elit. Est commodi, dolore a nulla necessitatibus deleniti voluptas delectus beatae cupiditate cumque!</p>
-                <h4 style="font-family: 'Source Sans Pro', sans-serif; float: left; font-style: italic; margin-left: 4%;">location</h4>
-                <h4>phone #</h4>
-                <div style="margin-bottom: 30px;"></div>
-            </div>
-            
-        </center>
-        </div>
         
         <center>
 <!-- this is the layout for all your projects, edit to fit your needs -->
